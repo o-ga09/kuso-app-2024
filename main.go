9dcdd7b2a9a76b01541ad0e13dc59396a94a31bb
o-ga09 <taitiabe1997@gmail.com>
2024-11-24 23:49:11 +0900

    refactor

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/o-ga09/kuso-app-2024/internal/genkit"
+	"github.com/o-ga09/kuso-app-2024/internal/notifier"
+	"github.com/o-ga09/kuso-app-2024/internal/parser"
 )
 
 const (
@@ -16,40 +20,40 @@ func main() {
 	ctx := context.Background()
 
 	// フィードを取得する
-	feed, err := ParseAtomFeed(ctx)
+	feed, err := parser.ParseAtomFeed(ctx)
 	if err != nil {
 		fmt.Println("Error parsing the feed")
 		os.Exit(1)
 	}
 
 	// 今日の日付と比較して、最新のフィードを取得する
-	latestFeed, err := CompareDiffFeed(ctx, feed)
-	if err != nil && !errors.Is(err, ErrNotLatestFeed) {
+	latestFeed, err := parser.CompareDiffFeed(ctx, feed)
+	if err != nil && !errors.Is(err, parser.ErrNotLatestFeed) {
 		log.Fatalf("Error comparing the feed: %v", err)
 	}
 
-	var message SlackMessage
+	var message notifier.SlackMessage
 	// ブログ記事をGeminiで要約する
-	if errors.Is(err, ErrNotLatestFeed) {
+	if errors.Is(err, parser.ErrNotLatestFeed) {
 		// Slackに通知する
-		message = SlackMessage{
+		message = notifier.SlackMessage{
 			Text: "<!channel>\n 新しいブログ記事はありません",
 		}
 	} else {
-		summary, err := SummarizeBlog(ctx, latestFeed.Link.Href)
+		summary, err := genkit.SummarizeBlog(ctx, latestFeed.Link.Href)
 		if err != nil {
 			log.Fatalf("Error summarizing the blog: %v", err)
 		}
 		// Slackに通知する
-		message = SlackMessage{
+		message = notifier.SlackMessage{
 			Text: "<!channel>\n ⭐️ 新しいブログ記事が投稿されました！ ⭐️\n\n ======== \n" + summary + "========",
 		}
 	}
 
 	// HTTPステータスが200以外の場合のみ3回リトライする
 	for i := 0; i < RETRY_COUNT; i++ {
-		err = SendSlackNotification(ctx, message)
-		if err == nil || !errors.Is(err, ErrHTTPStatusNotOK) {
+		err = notifier.SendSlackNotification(ctx, message)
+		if err == nil || !errors.Is(err, notifier.ErrHTTPStatusNotOK) {
 			break
 		}
 	}
