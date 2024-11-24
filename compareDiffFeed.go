72198358b2dbd5e029319aae2ef427c9168f8a3d
o-ga09 <taitiabe1997@gmail.com>
2024-11-24 14:36:38 +0900

    Atom Feedから最新のブログを取得する

---
+++ compareDiffFeed.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotLatestFeed = errors.New("not latest feed")
+)
+
+func CompareDiffFeed(ctx context.Context, latestFeed *AtomFeed) (*Entry, error) {
+	// 今日の日付と比較して、今日以降のフィードを取得
+	isLatest, err := isCompareDiffFeed(ctx, &latestFeed.Entries[0])
+	if err != nil {
+		return nil, err
+	}
+
+	if isLatest {
+		return &latestFeed.Entries[0], nil
+	}
+	return nil, ErrNotLatestFeed
+}
+
+func isCompareDiffFeed(_ context.Context, feedEntity *Entry) (bool, error) {
+	// ctxtime(https://github.com/newmo-oss/ctxtime)を使いたい
+	// が、今回は、ctxtimeを使わずに、time.Timeを使う
+	// now := time.Now()
+
+	// debug
+	now := time.Date(2024, 11, 11, 0, 0, 0, 0, time.UTC)
+	if feedEntity.Published.Year() >= now.Year() && feedEntity.Published.Month() >= now.Month() && feedEntity.Published.Day() >= now.Day() {
+		return true, nil
+	}
+	return false, nil
+}
